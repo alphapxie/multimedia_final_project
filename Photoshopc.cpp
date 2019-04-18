268d6d888e470c1fe8ced32f5b3deb3353bca666
Grégoire <gdedinechin@hotmail.fr>
2019-04-18 10:58:30 +0200

    Add dilatation, erosion, cannyedge

--- Photoshopc.cpp
+++ Photoshopc.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "Photoshopc.h"
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+
+
+Mat Photoshopc::dilatation(int iteration){
+    Mat dest;
+    dilate(this->image, dest, 0, Point (-1,1), iteration, BORDER_REFLECT101, morphologyDefaultBorderValue());
+}
+
+Mat Photoshopc::erosion(int iteration){
+    Mat dest;
+    erode(this->image, dest, 0, Point (-1,1), iteration, BORDER_REFLECT101, morphologyDefaultBorderValue());
+}
+
+Mat Photoshopc::cannyEdgeDetection(double thresh1=100, double thresh2=200, int apertureSize=3){
+    Mat dest;
+    Canny(this->image, dest, thresh1, thresh2, apertureSize, false);
+}
--- Photoshopc.h
+++ Photoshopc.h
@@ -2,6 +2,11 @@
 // Created by victo on 18/04/2019.
 //
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
 #ifndef MULTIMEDIA_FINAL_PROJECT_PHOTOSHOPC_H
 #define MULTIMEDIA_FINAL_PROJECT_PHOTOSHOPC_H
 
@@ -11,13 +16,13 @@ class Photoshopc {
     Mat image;
 public:
     Photoshopc();
-    Mat morphologicalOperators();
-    Mat erosion();
+    Mat dilatation(int);
+    Mat erosion(int);
     Mat resize();
     Mat luminosity();
     Mat panorama();
     Mat stitching();
-    Mat cannyEdgeDetection();
+    Mat cannyEdgeDetection(double, double, int);
 
 };
 
